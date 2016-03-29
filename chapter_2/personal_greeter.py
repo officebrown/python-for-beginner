79aa51997fd117d4f40caae1775f92fb13ad96be
Jim Brown <jim@officebrown.com>
2016-03-29 11:47:52 -0400

    added new files: greeter.py, personal_greeter.py, silly_strings.py, word_problems.py

---
+++ chapter_2/personal_greeter.py
@@ -0,0 +1,18 @@
+# Personal Greeter
+# Demonstrates getting user input
+
+name = input("Hi.  What's your name? ")
+
+print(name)
+
+print("Hi,", name)
+
+name2 = input("Hi. And what's YOUR name? ")
+
+print(name2)
+
+print("Hi, ", name2)
+
+print("\n\nHello " + name + " and " + name2 + "!")
+
+input("\n\nPress the enter key to exit.")

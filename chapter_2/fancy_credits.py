d888a6b937b0155c7326d61c133c07d0d21a461d
Jim Brown <jim@officebrown.com>
2016-03-29 10:44:43 -0400

    added new file: fancy_credits.py

---
+++ chapter_2/fancy_credits.py
@@ -0,0 +1,15 @@
+# Fancy Credits
+# Demonstrates escape sequences
+
+print("\t\t\tFancy Credits")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tMichael Dawson")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\nSpecial thanks goes out to:")
+input("My hair stylist, Henry \'The Great,\' who never says \"can\'t\"")
+
+# sound the system bell
+print("\a")
+
+input("\n\nPres the enter key to exit.")
